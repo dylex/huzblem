17588a955e7331cc0152b1d2739b4c8b21d9a252
Dylan Simon <dylan@dylex.net>
2014-03-22 20:54:26 -0400

    Expose and disable local storage and other settings

--- Bind.hs
+++ Bind.hs
@@ -267,6 +267,7 @@ commandBinds = Map.fromAscList $
   , ((modMod1, "f"),	toggleBlock "iframe")
   , ((modMod1, "h"),	favorites . fromMaybe 50 =<< countMaybe)
   , ((modMod1, "i"),	toggleBlock "img")
+  , ((modMod1, "l"),    toggleOrCount "enable_local_stoage" onOff)
   , ((modMod1, "m"),    promptMark False)
   , ((modMod1, "p"),    toggleOrCount "enable_private" onOff)
   , ((modMod1, "s"),	toggleBlock "script")
